d38a0f4679f30b67cdbcfac798fab0fa46affb44
mangdo <70243735+mangdo@users.noreply.github.com>
2021-06-29 00:42:00 +0900

    first first settings

---
+++ src/main/java/com/example/mydevboard/MyDevBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.mydevboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDevBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyDevBoardApplication.class, args);
+    }
+
+}
